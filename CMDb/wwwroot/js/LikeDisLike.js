1b0bdafa43caba17f12f336740767705851fe304
Tobias Hall <tobbe085@gmail.com>
2020-10-28 15:38:23 +0100

    -skapade tooglePlot för att lyfta ut den koden till en egen fil

--- CMDb/wwwroot/js/LikeDisLike.js
+++ CMDb/wwwroot/js/LikeDisLike.js
@@ -3,16 +3,18 @@
     //const cmdb = new XMLHttpRequest()
     //const statement = 'like'
     //const url = `https://localhost:44313/api/${imdbId}/${statement}`
-    //cmdb.open("Get", url)
+    //cmdb.open("Get", url, true)
     //cmdb.send()
     clickedElement = clickedElement.textContent++
+    const button = document.querySelector('#btn-dislike')
+    button.disabled = true
 }
 function disLikeMovie(clickedElement, imdbId) {
 
     //const cmdb = new XMLHttpRequest()
     //const statement = 'dislike'
     //const url = `https://localhost:44313/api/${imdbId}/${statement}`
-    //cmdb.open("Get", url)
+    //cmdb.open("Get", url, true)
     //cmdb.send()
     clickedElement = clickedElement.textContent++
 }
