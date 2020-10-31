19359bc55c2f3981b2b6fd5a03f9e1f08e6ea9e8
Tobias Hall <tobbe085@gmail.com>
2020-10-31 15:35:10 +0100

    -Ändrar bildlänkar till asp-action osv. -fixat tooglePlot så den loopar -Sädade i css:en för knapparna

--- CMDb/wwwroot/js/togglePlot.js
+++ CMDb/wwwroot/js/togglePlot.js
@@ -1,15 +1,39 @@
-﻿const moreBtn = document.querySelector('.more')
-const plotDiv = document.querySelector('.plot-div')
+﻿//const moreBtn = document.querySelector('.more')
+//const plotDiv = document.querySelector('.plot-div')
 
-moreBtn.addEventListener('click', function (event) {
-    event.stopPropagation()
-    event.preventDefault()
-    if (moreBtn.text == 'Read more') {
-        plotDiv.style.height = '100%'
-        moreBtn.textContent = 'Collapse'
-    }
-    else {
-        plotDiv.style.height = '50px'
-        moreBtn.textContent = 'Read more'
-    }
-})
+//moreBtn.addEventListener('click', function (event) {
+//    event.stopPropagation()
+//    event.preventDefault()
+//    if (moreBtn.text == 'Read more') {
+//        plotDiv.classList.toggle('is-active')
+
+//        //plotDiv.style.height = '100%'
+//        moreBtn.textContent = 'Collapse'
+//    }
+//    else {
+//        plotDiv.classList.toggle('is-active')
+//        //plotDiv.style.height = '50px'
+//        moreBtn.textContent = 'Read more'
+//    }
+//})
+
+const moreBtn = document.querySelectorAll('.more')
+const plotDiv = document.querySelectorAll('.plot-div')
+
+for (let index = 0; index < moreBtn.length; index++) {
+    moreBtn[index].addEventListener('click', function (event) {
+        //event.stopPropagation()
+        event.preventDefault()
+        if (moreBtn[index].text == 'Read more') {
+            plotDiv[index].classList.toggle('is-active')
+            //plotDiv.style.height = '100%'
+            moreBtn[index].textContent = 'Collapse'
+        }
+        else {
+            plotDiv[index].classList.toggle('is-active')
+            //plotDiv.style.height = '50px'
+            moreBtn[index].textContent = 'Read more'
+        }
+    })    
+}
+
