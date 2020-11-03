794a202531c5acb97b3018c87a44223c6b3dfef3
Tobias Hall <tobbe085@gmail.com>
2020-11-03 15:37:31 +0100

    -Städat ytterligare

--- CMDb/wwwroot/js/togglePlot.js
+++ CMDb/wwwroot/js/togglePlot.js
@@ -1,37 +1,16 @@
-﻿//const moreBtn = document.querySelector('.more')
-//const plotDiv = document.querySelector('.plot-div')
-
-//moreBtn.addEventListener('click', function (event) {
-//    event.stopPropagation()
-//    event.preventDefault()
-//    if (moreBtn.text == 'Read more') {
-//        plotDiv.classList.toggle('is-active')
-
-//        //plotDiv.style.height = '100%'
-//        moreBtn.textContent = 'Collapse'
-//    }
-//    else {
-//        plotDiv.classList.toggle('is-active')
-//        //plotDiv.style.height = '50px'
-//        moreBtn.textContent = 'Read more'
-//    }
-//})
-
+﻿
 const moreBtn = document.querySelectorAll('.more')
 const plotDiv = document.querySelectorAll('.plot-div')
 
 for (let index = 0; index < moreBtn.length; index++) {
     moreBtn[index].addEventListener('click', function (event) {
-        //event.stopPropagation()
         event.preventDefault()
         if (moreBtn[index].text == 'Read more') {
-            plotDiv[index].classList.toggle('is-active')
-            //plotDiv.style.height = '100%'
+            plotDiv[index].classList.toggle('is-active')            
             moreBtn[index].textContent = 'Collapse'
         }
         else {
             plotDiv[index].classList.toggle('is-active')
-            //plotDiv.style.height = '50px'
             moreBtn[index].textContent = 'Read more'
         }
     })    
